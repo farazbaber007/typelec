21067621d673c2bddb7b1a16000be2223c6a21f6
farazbaber007 <farazbaber007@gmail.com>
2023-11-18 22:54:41 +0500

    update

--- lec_1/index.ts
+++ lec_1/index.ts
@@ -1,1 +1,5 @@
 console.log("Hello World. Thanks you sir Zia.");
+
+export {}; // its just for the safe side (defined variable will not conflict due to this command)
+
+// tsc -w ( it will automatically run tsc command, but we have to open 2nd terminal for writing command) 
